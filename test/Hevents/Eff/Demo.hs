5ce4f5806d10a8da1aa886355a8dd7c12fe54aee
Arnaud Bailly <arnaud.oqube@gmail.com>
2016-05-09 09:34:10 +0200

    demo: server test

--- test/Hevents/Eff/Demo.hs
+++ test/Hevents/Eff/Demo.hs
@@ -20,6 +20,7 @@ import           Data.Serialize             (Serialize, get, put)
 import           Data.Typeable
 import           Data.Void
 import           Hevents.Eff                as W
+import           Network.Wai.Handler.Warp   as W
 import           Prelude                    hiding ((.))
 import           Servant
 import           Servant.Client
@@ -46,3 +47,13 @@ counterServer = getCounter
     increment  = const undefined
     decrement  = const undefined
 
+-- * A Test
+apiSpec :: Spec
+apiSpec = describe "Counter Server" $ do
+  it "should return 0 as initial state of counter" $ do
+    s <- async $ W.run 8082 $ serve counterApi counterServer
+    Right n <- (runEitherT $ getCounter) `finally` cancel s
+
+    n `shouldBe` 0
+      where
+        getCounter :<|> _  :<|> _ = client counterApi (BaseUrl Http "localhost" 8082)
