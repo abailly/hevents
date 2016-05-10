e0744e5dcbbb84cfab17fd2d0ca3373736890cb6
Arnaud Bailly <arnaud.oqube@gmail.com>
2016-05-10 15:40:23 +0200

    demo2: define property and arbitrary instances

--- test/Hevents/Eff/Demo.hs
+++ test/Hevents/Eff/Demo.hs
@@ -38,3 +38,19 @@ import           Test.QuickCheck.Monadic    as Q
 counterSpec :: Spec
 counterSpec = describe "Counter model" $ do
   it "should apply result of commands given it respects bounds" $ property $ prop_shouldActAndApplyCommandsRespectingBounds
+
+-- * The commands that can act on the model
+instance Arbitrary (Command Counter) where
+  arbitrary = oneof [ Increment <$> number
+                    , Decrement <$> number
+                    ]
+    where
+      number = choose (0,10)
+
+-- * First property, test all commands against pristine model
+prop_shouldActAndApplyCommandsRespectingBounds :: Command Counter -> Bool
+prop_shouldActAndApplyCommandsRespectingBounds c@(Increment i) = let OK result = init `act` c
+                                                                 in init `apply` result == Counter i
+prop_shouldActAndApplyCommandsRespectingBounds c@(Decrement i) = let counter = Counter 10
+                                                                     OK result = counter `act` c
+                                                                 in counter `apply` result == Counter (10 - i)
