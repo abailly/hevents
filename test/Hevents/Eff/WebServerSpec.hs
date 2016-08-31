b94b3de0ee56b405c48dbc2b7bf8972f051008b8
Arnaud Bailly <arnaud.oqube@gmail.com>
2016-08-31 12:23:52 +0200

    add (ugly) timeout to webserver test, waiting for it to spin up

--- test/Hevents/Eff/WebServerSpec.hs
+++ test/Hevents/Eff/WebServerSpec.hs
@@ -8,22 +8,23 @@ module Hevents.Eff.WebServerSpec
        where
 
 import           Control.Category
+import           Control.Concurrent
 import           Control.Concurrent.Async
 import           Control.Concurrent.STM
-import qualified Control.Eff                as E
-import           Control.Eff.Lift           as E hiding (lift)
+import qualified Control.Eff              as E
+import           Control.Eff.Lift         as E hiding (lift)
 import           Control.Exception
 import           Control.Monad.Except
-import           Data.Functor               (void)
+import           Data.Functor             (void)
 import           Data.Proxy
 import           Data.Typeable
 import           Data.Void
-import           Hevents.Eff                as W hiding (effect)
+import           Hevents.Eff              as W hiding (effect)
 import           Hevents.Eff.TestModel
-import           Prelude                    hiding ((.))
+import           Network.HTTP.Client      (defaultManagerSettings, newManager)
+import           Prelude                  hiding ((.))
 import           Servant
 import           Servant.Client
-import           Network.HTTP.Client        (newManager, defaultManagerSettings)
 import           Test.Hspec
 
 
@@ -54,14 +55,14 @@ spec = describe "Web Server Effect" $ do
 
     mgr <- newManager defaultManagerSettings
 
-    s <- W.runWebServer 8082 testAPI (effect storage model) handler
-    
+    s <- W.runWebServer 8083 testAPI (effect storage model) handler
+    threadDelay 500000  -- wait for server to be up
     n <- runExceptT (runClient 42 mgr baseUrl) `finally` cancel s
-    
+
     either (error . show) (`shouldBe` 42) n
 
       where
-        baseUrl = (BaseUrl Http "127.0.0.1" 8082 "")
+        baseUrl = (BaseUrl Http "127.0.0.1" 8083 "")
         prepareContext = (,) <$>
           newTVarIO (W.init :: TestModel) <*>
           atomically W.makeMemoryStore
