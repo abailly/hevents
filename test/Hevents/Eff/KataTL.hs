f00779e81243b7771bf566566d18608f93b1063a
Arnaud Bailly <arnaud.oqube@gmail.com>
2017-02-13 12:35:14 +0100

    update tests

--- test/Hevents/Eff/KataTL.hs
+++ test/Hevents/Eff/KataTL.hs
@@ -1,22 +1,33 @@
-{-# LANGUAGE DataKinds #-}
-{-# LANGUAGE PolyKinds #-}
+{-# LANGUAGE AllowAmbiguousTypes, DataKinds, PolyKinds #-}
 import           Data.Proxy
-import           Data.String
+import           Data.Text          (Text)
+import           Data.Text.Encoding
 import           GHC.TypeLits
 import           Test.Hspec
 import           Test.QuickCheck
 
-data Digit = I
-           | IV
-           | V
-           | IX
-           | X
+-- 3 different versions of the "same" structure
+-- We only care about
 
-type family FromRoman (r :: [Digit]) :: Nat where
-  FromRoman '[]        = 0
-  FromRoman ('I ': ds) = 1 + FromRoman ds
-  FromRoman '[IV]      = 4
-  FromRoman ('V ': ds) = 5 + FromRoman ds
-  FromRoman '[IX]      = 9
-  FromRoman ('X ': ds) = 10 + FromRoman ds
+data Obj1 = Obj1 { f11 :: Int, f12 :: Text }
+
+-- version 2 transforms a primitive field into a data type
+data F1 = F1 { ff1 :: Int, ff2 :: Text }
+data Obj2 = Obj2 { f21 :: F1, f22 :: Text }
+
+-- version 3 transforms the nested data type
+data F2 = F2 { ff12 :: Text }
+data Obj3 = Obj3 { f31 :: F2, f32 :: Text }
+
+-- final version, same structure than version 3
+data F = F { f' :: Text }
+data Obj = Obj { f :: F, g :: Text }
+
+
+data Ver = Ver Nat
+
+-- Migration type class
+-- Parameterized with 2 versions and a functor
+class Migrate (v :: Ver) (v' :: Ver) f where
+  migrate :: f v a -> f v' a
 
--- test/Hevents/Eff/Version.hs
+++ test/Hevents/Eff/Version.hs
@@ -1,8 +1,8 @@
-{-# LANGUAGE GADTs, AllowAmbiguousTypes, DataKinds, PolyKinds #-}
+{-# LANGUAGE AllowAmbiguousTypes, DataKinds, GADTs, PolyKinds #-}
 import           Data.ByteString    (ByteString)
 import           Data.Proxy
 import           Data.Serialize
-import           Data.Text          (pack,Text)
+import           Data.Text          (Text, pack)
 import           Data.Text.Encoding
 import           GHC.TypeLits
 import           Test.Hspec
@@ -15,53 +15,53 @@ instance Serialize Text where
 
 -- | A class instantiating a serializer/deserializer for some version
 class Versionable (v :: Nat) a where
-  read  :: Proxy v -> ByteString -> Either String a
-  write :: Proxy v -> a          -> ByteString
+  reader :: Proxy v -> Get a
+  writer :: Proxy v -> a -> Put
 
-data Reader (v :: Nat) a where
-  ReadVersion :: (ByteString -> Versioned v a) -> Reader v a
+-- | A structure representing some type `a` as serialized to a `ByteString`
+-- This structure ultimately holds a tree of `Get x`
+data Versioned a where
+  (:$:) ::     (a -> b) -> Versioned  a -> Versioned  b
+  (:*:) :: Versioned  (a -> b) -> Versioned  a -> Versioned  b
+  Atom  :: Get a                 -> Versioned  a
+  Cast  :: Versioned a               -> Versioned  a
   
-          
 -- | Current version is a "global" constraint for some type
-type family CurrentVersion a :: Nat
-type instance CurrentVersion Obj3 = 3
+type family CurrentVersion :: Nat
+type instance CurrentVersion = 3
 
-load :: (Versionable (CurrentVersion a) a) => ByteString -> Either String [a]
-load _ = undefined
+class VersionUpTo (v :: Nat) a
 
--- A Versioned deserializer for type `a`
--- allows deserializing instances of `a` from version `v`, reifies applicative structure
-data Versioned (v :: Nat) a where
-  (:$:) ::             (a -> b) -> Versioned v a -> Versioned v b
-  (:*:) :: Versioned v (a -> b) -> Versioned v a -> Versioned v b
-  Atom  :: Get a                                 -> Versioned v a
-  Cast  :: Versioned v' a                        -> Versioned v a
-  -- ^Used for asserting that version `v` can be converted to version `v'`
+instance (Versionable 1 a) => VersionUpTo 1 a
+
+instance (Versionable v  a, VersionUpTo (v - 1) a) => VersionUpTo v a
+
+load :: (VersionUpTo CurrentVersion a) => ByteString -> Either String [a]
+load = runGet loadGetter
+  where
+    loadGetter = sequence $ repeat $ do
+      v <- getInt32be
+      case v of
+        1 -> reader (Proxy :: Proxy 1)
+        2 -> reader (Proxy :: Proxy 2)
+        3 -> reader (Proxy :: Proxy 3)
+        n -> fail $ "unknown version " ++ show n
+
   
 infixl 4 :$:
 infixl 4 :*:
 
-doGet :: Versioned v a -> Get a
+doGet :: Versioned  a -> Get a
 doGet (Atom g)   = g
-doGet (Cast v)    = doGet v
 doGet (f :$: v)   = f <$> doGet v
 doGet (f :*: v)   = doGet f <*> doGet v
 
--- | Transforms a `Versioned v a` tree at some position
-atIndex :: Int -> (a -> b) -> Versioned v a -> Versioned v' b
+-- | Transforms a `Versioned  a` tree at some position
+atIndex :: Int -> (a -> b) -> Versioned  a -> Versioned  b
 atIndex 0 f (Atom g)  = Atom (f <$> g)
 atIndex 0 f (g :*: v) = Cast (atIndex 0 (f.) g :*: v)
 atIndex 0 f (g :$: v) = Cast $ (f . g) :$: v 
 atIndex n _         _ = error $ "Cannot transform Atom at index " ++ show n ++ " greater than 0"
-
-class Migrator (v :: Nat) (v' :: Nat) a m where
-  migrate :: (Versionable v a, Versionable v' a) => m v v' a -> Versioned v' a
-    
-data Migrate (v :: Nat) (v' :: Nat) a where
-  Migrate :: Versioned v b -> Versioned v' (b -> a) -> Migrate v v' a
-
-instance Migrator v v' a Migrate where
-  migrate (Migrate v f) = f :*: Cast v
 
 -- 3 different versions of the "same" structure
 
@@ -92,59 +92,50 @@ instance Serialize Obj2 where
 data F2 = F2 { ff12 :: Text }
   deriving (Show)
 
+instance Serialize F2 where
+  get = F2 <$> get
+  put F2{..} = put ff12
+  
 data Obj3 = Obj3 { f31 :: F1, f32 :: F2 }
   deriving (Show)
 
-getint :: Versioned v Int
+getint :: Versioned  Int
 getint = Atom (get :: Get Int)
 
-gettext :: Versioned v Text
+gettext :: Versioned  Text
 gettext = Atom (get :: Get Text)
 
-geto1 :: (Int -> Text -> Obj3) -> Versioned 1 Obj3
+geto1 :: (Int -> Text -> Obj3) -> Versioned Obj3
 geto1 f = f :$: getint :*: Atom get
 
 fromInt :: Int -> F1
 fromInt = flip F1 ""
 
-vf1 :: Migrate 1 2 F1
-vf1 = Migrate getint (Atom (pure fromInt))
-
 fromText :: Text -> F2
 fromText = F2
 
-vf2 :: Migrate 2 3 F2
-vf2 = Migrate gettext (Atom (pure fromText))
-
-geto2 :: (F1 -> Text -> Obj3) -> Versioned 2 Obj3
+getf2 :: (Text -> F2) -> Versioned F2
+getf2 f = f :$: Atom get 
+  
+geto2 :: (F1 -> Text -> Obj3) -> Versioned Obj3
 geto2 f = Cast $ geto1 (f.fromInt)
 
-geto3 :: (F1 -> F2 -> Obj3) -> Versioned 3 Obj3
-geto3 k = k :$: Atom get :*: Cast (migrate vf2)
-
-instance Versionable 1 Obj1 where
-  read    = undefined
-  write _ = runPut . put
-  
-instance Versionable 2 Obj2 where
-  read    = undefined
-  write _ = runPut . put
-
 instance Versionable 1 Obj3 where
-   read _  = runGet $ doGet (geto1 (\ i t -> Obj3 (fromInt i) (fromText t)))
-   write _ = undefined
+  reader _ = doGet $ Obj3 :$: (fromInt :$: getint) :*: (fromText :$: Atom get)
+  writer _ = writer (Proxy :: Proxy 3)
 
 instance Versionable 2 Obj3 where
-   read _  = runGet $ doGet (geto2 $ \ i t -> Obj3 (fromInt i) (fromText t))
-   write _ = undefined
+  reader _ = doGet $ Obj3 :$: Atom get :*: (fromText :$: Atom get)
+  writer _ = writer (Proxy :: Proxy 3)
+   
+instance Versionable 3 Obj3 where
+   reader _          = doGet $ Obj3 :$: Atom get :*: getf2 F2
+   writer _ Obj3{..} = put f31 >> put f32
    
 -- a bytestring for version 
 bs1 :: ByteString
-bs1 = w (Obj1 12 "foo")
-  where
-    w = write (Proxy :: Proxy 1)
+bs1 = runPut $ put (Obj1 12 "foo")
 
 bs2 :: ByteString
-bs2 = w (Obj2 (F1 12 "bar") "foo")
-  where
-    w = write (Proxy :: Proxy 2)
+bs2 = runPut $ put (Obj2 (F1 12 "bar") "foo")
+
